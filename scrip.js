c537e4af9bf081771bd6d540959266a43b9d46f6
Nathan Rocha <neroite71@hotmail.com>
2025-05-28 22:38:27 -0300

    Deixando o código dinâmico e aplicando funcionalidae nos botões Previous e Next

--- scrip.js
+++ scrip.js
@@ -1,11 +1,87 @@
 const songName = document.getElementById("song-name"); // Const pois o id da música não vai mudar
-const song = document.getElementById("audio"); //
+const song = document.getElementById("audio");
+const bandName = document.getElementById("band-name"); 
+const cover = document.getElementById("cover");
 const play = document.getElementById("play");
+const previous = document.getElementById("previous");
+const next = document.getElementById("next");
 
-songName.innerText = "Fé" // InnerText acessa e modifica o conteúdo visível do elemento HTML
-
-function playSong() {
-    song.play();
+const deepEletronic = {
+    songName: "Deep Electronic",
+    artist: "Free Electronic Sounds",
+    file : "deep_electronic",
 }
 
-play.addEventListener("click", playSong);
+const brainImplant = {
+    songName: "Brain Implant",
+    artist: "Free Implant Sounds",
+    file : "brain_implant",
+}
+
+const experimentalCinematic = {
+    songName: "Experimental Cinematic",
+    artist: "Free Cinematic Sounds",
+    file : "Experimental_Cinematic",
+}
+
+const playlist = [brainImplant, deepEletronic, experimentalCinematic];
+let index = 0;
+
+let isPlaying = false;
+function playSong() {
+    play.querySelector(".bi").classList.remove("bi-play-circle-fill");    
+    play.querySelector(".bi").classList.add("bi-pause-circle-fill");
+    song.play();
+    isPlaying = true;
+}
+
+function pauseSong() {
+    play.querySelector(".bi").classList.remove("bi-pause-circle-fill");    
+    play.querySelector(".bi").classList.add("bi-play-circle-fill");
+    song.pause();
+    isPlaying = false;
+}
+
+function playPauseDetect(){
+    if(isPlaying === true){
+        pauseSong();
+    }
+    else{
+        playSong();
+    }
+}
+
+function initializeSong(){
+    cover.src = `images/${playlist[index].file}.jpg`;
+    song.src = `songs/${playlist[index].file}.mp3`;
+    songName.innerText = playlist[index].songName;
+    bandName.innerText = playlist[index].artist;
+}
+
+function previousSong() {
+    if(index === 0) {
+        index = playlist.length - 1; // 
+    }
+    else{
+        index--;
+    }
+    initializeSong();
+    playSong();
+}
+
+function nextSong() { 
+    if(index === playlist.length - 1) {
+        index = 0; // 
+    }
+    else{
+        index++;
+    }
+    initializeSong();
+    playSong();
+}
+
+initializeSong();
+
+play.addEventListener("click", playPauseDetect);
+previous.addEventListener("click", previousSong);
+next.addEventListener("click", nextSong);
